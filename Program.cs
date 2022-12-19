dc8b87912bb53e37d5ec540766423e8d4904b96e
Cristiane Guimarães Fröhlich <cristiane_g_frohlich@hotmail.com>
2022-12-19 15:20:31 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+namespace LeitorPdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ExtraiTextoPdf("teste.pdf"));
+            Console.ReadKey();
+        }
+
+        static string ExtraiTextoPdf(string nomeDoArquivo)
+        {
+            string result = null; //string de retorno
+            PdfReader pdfReader = new PdfReader(nomeDoArquivo);
+            PdfDocument pdfDoc = new PdfDocument(pdfReader);
+            for(int page = 1; page <= pdfDoc.GetNumberOfPages(); page++) //lendo o pdf e utilizando o for, pois ele pode conter várias páginas
+            {
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy(); //tratando a iteração
+                string conteudo = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(page), strategy);
+                result += conteudo;
+            } 
+            pdfDoc.Close();
+            pdfReader.Close();
+            return result;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LeitorPdf
+ faz a leitura do PDF e mostra no console
